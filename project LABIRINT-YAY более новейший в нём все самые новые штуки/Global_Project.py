b151716d047bd53ede401ed4a578dd52e90c2811
hardestov <105546169+hardestov@users.noreply.github.com>
2022-07-06 17:16:48 +0100

    Update Global_Project.py

--- project LABIRINT-YAY более новейший в нём все самые новые штуки/Global_Project.py
+++ project LABIRINT-YAY более новейший в нём все самые новые штуки/Global_Project.py
@@ -173,7 +173,7 @@ for i in map1:
 
 #Создание врагов, и подобного
 #player(Координаты x, координаты y, ширина, высота, скорость игрока, картинка игрока, коорднаты до кудого может дойти игрок. Первые 2, это начало. Вторые 2 координаты, это кwонечные)
-player = Player(200, 350, 20, 32, 5, 0, 0, 450, 580, player_right_1, player_right_2, player_left_1, player_left_2, player_stay, player_down_1, player_down_2, player_up_1, player_up_2)
+player = Player(200, 350, 20, 32, 5, 0, 0, 800, 580, player_right_1, player_right_2, player_left_1, player_left_2, player_stay, player_down_1, player_down_2, player_up_1, player_up_2)
 
 #Цикл игры
 while game:
